d0f1faa73f245657704b0da37d44e5ef95658ff6
id-edm <ivandubochit@gmail.com>
2023-10-10 19:35:24 +0400

    feat: add paint board

--- random-game/main.js
+++ random-game/main.js
@@ -1,23 +1,41 @@
 
 // select canvas
 const cvs = document.getElementById('canvas');
+const ctx = cvs.getContext('2d');
 
 // add a border to cvs
-cvs.style.border = '2px solid black';
+cvs.style.border = '1px solid black';
+
+// cvs  measuring
+const width = cvs.width;
+const height = cvs.height;
 
 // game const and let
 const FPS = 1000 / 20;
 let gameLoop;
 
+//colors game
+let boardColor = 'blue'
+
+//рисуем поле для змеи
+function paintBoard () {
+    ctx.fillStyle = boardColor
+    ctx.fillRect(0, 0, width, height)
+}
+
 // game loop
 function cadre() {
-    console.log('test')
-    let gameLoop
+    paintBoard();
+    //paintFood();
+    //movSnake();
+    //paintSnake();
+    //renderScore();
 
-// проверка на столкновение со стеной или телом змеи
+// проверка на столкновение со стеной и телом змеи
     if(hitWall() || hitSelf())  {
         clearInterval
         gameOver()
+        
     }
 }
 
